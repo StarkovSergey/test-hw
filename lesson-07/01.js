2c8cae1a3b22890d61641c582c67efe2d07ee4f1
Starkov Sergey <starkovsr@gmail.com>
2024-06-25 22:20:30 +0300

    finish task-1

---
+++ lesson-07/01.js
@@ -0,0 +1,6 @@
+/*
+  Нужно написать функцию capitalizeWords, которая принимает строку и делает так, чтобы каждое слово начиналось с заглавной буквы.
+  Например, если передать строку "hello world from javascript", функция должна вернуть "Hello World From JavaScript"
+*/
+
+function capitalizeWords() {}
